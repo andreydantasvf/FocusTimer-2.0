45fb73982556483757c29c280166508f3c6dba9e
Andrey Dantas <andreydantasvf@gmail.com>
2022-11-18 21:10:02 -0300

    Pegando todos os elementos do HTML com Js

---
+++ js/elements.js
@@ -0,0 +1,29 @@
+const buttonPlay = document.querySelector('button.play')
+const buttonPause = document.querySelector('button.pause')
+const buttonStop = document.querySelector('button.stop')
+const buttonSet = document.querySelector('button.set')
+const buttonIncrease = document.querySelector('button.increase')
+const buttonDecrease = document.querySelector('button.decrease')
+
+const buttonSoundForest = document.querySelector('button#forest-sound')
+const buttonSoundRain = document.querySelector('button#rain-sound')
+const buttonSoundCoffeeShop = document.querySelector('button#coffeeShop-sound')
+const buttonSoundFireplace = document.querySelector('button#fireplace-sound')
+
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+export {
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet,
+    buttonIncrease,
+    buttonDecrease,
+    minutesDisplay,
+    secondsDisplay,
+    buttonSoundForest,
+    buttonSoundRain,
+    buttonSoundCoffeeShop,
+    buttonSoundFireplace
+}
